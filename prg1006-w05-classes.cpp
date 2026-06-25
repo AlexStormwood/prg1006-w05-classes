ac87147d5eaabf7d9a1fe9645fa543fb58841597
Alex <alex.stormwood@bigfootds.com>
2026-06-25 14:54:20 +1000

    feat: public/private and getter/setter

--- prg1006-w05-classes.cpp
+++ prg1006-w05-classes.cpp
@@ -8,9 +8,27 @@ struct PlayerStruct {
 };
 
 class PlayerClass {
+// intentionally state private even though it's private by default
+// so that any other devs know that this is in purpose
+private: 
     unsigned __int8 health;
     unsigned int score;
     std::string name;
+
+public: 
+    // instead of redeclaring a variable to make it public,
+    // we should use getters and setters!
+
+
+    int getHealth() {
+        // explicit casting:
+        // return static_cast<int>(health);
+        // implicit casting:
+        return health;
+    }
+    void setHealth(unsigned __int8 newHealth){
+        health = newHealth;
+    }
 };
 
 
@@ -20,6 +38,7 @@ int main()
     PlayerClass classPlayer;
     structPlayer.health = 100;
     //classPlayer.health = 100; // error because by default, it's private
-
+    classPlayer.setHealth(100);
+    std::cout << classPlayer.getHealth() << std::endl;
 }
 
