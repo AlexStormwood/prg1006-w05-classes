5983e0cf8d1d1a003028eaffabc34dd304519bb0
Alex <alex.stormwood@bigfootds.com>
2026-07-02 14:41:36 +1000

    feat: file io basics

--- prg1006-w05-classes.cpp
+++ prg1006-w05-classes.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <fstream> // this enables file IO
+#include <string>
 
 // function to welcome the user to the app
 // literally just show a string to the screen
@@ -37,6 +38,7 @@ public:
         }
         
         name = newPlayerName;
+        loadPlayerName();
         std::cout << "Player has been instantiated! They have health of " << static_cast<int>(health) << std::endl;
     }
     // Initialisation list style:
@@ -44,6 +46,12 @@ public:
         name(newPlayerName), health(newPlayerHealth) {
         std::cout << "Player has been instantiated! They have health of " << static_cast<int>(health) << std::endl;
     }*/
+
+    ~PlayerClass() {
+        std::cout << name << " has been destroyed!" << std::endl;
+    }
+
+
 
     // instead of redeclaring a variable to make it public,
     // we should use getters and setters!
@@ -64,6 +72,29 @@ public:
         }
         
     }
+
+    void savePlayerName() {
+        // ofstream comes from fstream, make sure we include that!!!
+        // "#include <fstream>;" to make this work!!!
+        // ofstream by default will create the file if it doesn't exist
+        std::ofstream playerSaveFile("savedata.txt");
+        // If we don't provide "something", then "nothing" will be saved
+        // and the default ofstream config will replace all existing contents
+        // with the nothing or the something
+        playerSaveFile << name;
+        // Close the file stream to be extra safe about any other code
+        // accidentally editing the file
+        playerSaveFile.close();
+
+    }
+
+    void loadPlayerName() {
+        std::ifstream existingSaveFile("savedata.txt");
+        // "#include <string>;" to make the getline work:
+        std::getline(existingSaveFile, name);
+        std::cout << name << std::endl;
+    }
+
 };
 
 
@@ -80,6 +111,8 @@ int main()
     //classPlayer.health = 100; // error because by default, it's private
     classPlayer.setHealth(20);
     std::cout << classPlayer.getHealth() << std::endl;
+
+    classPlayer.savePlayerName();
 
     /*PlayerClass player2;
     player2.setHealth(50);*/
