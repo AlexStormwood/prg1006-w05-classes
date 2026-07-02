55743a109647e23f0f4a1371712d5590d82ef407
Alex <alex.stormwood@bigfootds.com>
2026-07-02 13:38:59 +1000

    feat: inline func, class constructor

--- prg1006-w05-classes.cpp
+++ prg1006-w05-classes.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 
+// function to welcome the user to the app
+// literally just show a string to the screen
+// nice and simple for inlining 
+
+inline void welcomeTheUser() {
+    std::cout << "Welcome to the app!" << std::endl;
+}
+
+
 struct PlayerStruct {
     unsigned __int8 health;
     unsigned int score;
@@ -16,9 +25,13 @@ private:
     std::string name;
 
 public: 
+    PlayerClass () {
+        health = 100;
+        std::cout << "Player has been instantiated! They have health of " << static_cast<int>(health) << std::endl;
+    }
+
     // instead of redeclaring a variable to make it public,
     // we should use getters and setters!
-
 
     int getHealth() {
         // explicit casting:
@@ -27,19 +40,30 @@ public:
         return health;
     }
     void setHealth(unsigned __int8 newHealth){
-        health = newHealth;
+        /*health = newHealth; */
+        if (newHealth > 100){
+            // do not set the health, health is already high enough
+        }
+        else {
+            health = newHealth;
+        }
+        
     }
 };
 
 
 int main()
 {
+    welcomeTheUser();
+    // inline keyword replaces the function call with the function contents:
+    // std::cout << "Welcome to the app!" << std::endl;
+
     // Instances of the struct and class:
     PlayerStruct structPlayer;
     PlayerClass classPlayer;
     structPlayer.health = 100;
     //classPlayer.health = 100; // error because by default, it's private
-    classPlayer.setHealth(100);
+    classPlayer.setHealth(20);
     std::cout << classPlayer.getHealth() << std::endl;
 
     PlayerClass player2;
