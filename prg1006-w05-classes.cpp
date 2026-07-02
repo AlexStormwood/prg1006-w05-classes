6eae702f0c924888753dd2445c31f5896b3ec9da
Alex <alex.stormwood@bigfootds.com>
2026-07-02 13:56:27 +1000

    feat: class constructor done

--- prg1006-w05-classes.cpp
+++ prg1006-w05-classes.cpp
@@ -25,10 +25,25 @@ private:
     std::string name;
 
 public: 
-    PlayerClass () {
-        health = 100;
+    // Classic constructor style:
+    PlayerClass (std::string newPlayerName, int newPlayerHealth) {
+        // health = 100;
+        // Sanitisation:
+        if (newPlayerHealth > 100) {
+            health = 100;
+        }
+        else {
+            health = newPlayerHealth;
+        }
+        
+        name = newPlayerName;
         std::cout << "Player has been instantiated! They have health of " << static_cast<int>(health) << std::endl;
     }
+    // Initialisation list style:
+    /*PlayerClass(std::string newPlayerName, int newPlayerHealth) :
+        name(newPlayerName), health(newPlayerHealth) {
+        std::cout << "Player has been instantiated! They have health of " << static_cast<int>(health) << std::endl;
+    }*/
 
     // instead of redeclaring a variable to make it public,
     // we should use getters and setters!
@@ -60,13 +75,13 @@ int main()
 
     // Instances of the struct and class:
     PlayerStruct structPlayer;
-    PlayerClass classPlayer;
+    PlayerClass classPlayer("Player Name Here", 200);
     structPlayer.health = 100;
     //classPlayer.health = 100; // error because by default, it's private
     classPlayer.setHealth(20);
     std::cout << classPlayer.getHealth() << std::endl;
 
-    PlayerClass player2;
-    player2.setHealth(50);
+    /*PlayerClass player2;
+    player2.setHealth(50);*/
 }
 
